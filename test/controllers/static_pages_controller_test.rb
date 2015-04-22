9f5883540100b3a09327782e4272751722d6457f
mohongshu <mo.hongshu@163.com>
2015-04-22 11:44:16 +0800

    完成基本布局以及相关路由

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,4 +20,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
   	assert_select "title", "关于 | #{base_title}" 
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "联系 | #{base_title}"
+  end
 end
