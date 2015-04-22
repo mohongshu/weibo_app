aee31a4027434109c3401f8300cd8bc9c42e1ea4
mohongshu <mo.hongshu@163.com>
2015-04-22 11:09:38 +0800

    对static-pages做小修改

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	#根据所在页面返回完整标题
 	def full_title(page_title = '')
-		base_title = "Ruby on Rails Tutorial Sample App"
+		base_title = "爱吐槽"
 		if page_title.empty?
 			base_title
 		else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  base_title = "爱吐槽"
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App" 
+    assert_select "title", "主页 | #{base_title}" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App" 
+    assert_select "title", "帮助 | #{base_title}" 
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App" 
+  	assert_select "title", "关于 | #{base_title}" 
   end
 end
