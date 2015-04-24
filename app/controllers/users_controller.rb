d0f8b98b76af14c0f5a22b702cebc3368b07ed35
mohongshu <mo.hongshu@163.com>
2015-04-24 19:37:07 +0800

    完成基本的用户注册

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#处理成功
+  		flash[:success] = "欢迎加入爱吐槽！"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, 
+  			:passwor_confirmation)
+  	end
 end
